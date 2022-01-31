c494f81d24a478cc94e394c78ae1a87bf0ac5f18
Tanakorn Phoochaliaw <tanakorn.dev@gmail.com>
2022-01-31 18:44:51 +0700

    Init project

---
+++ Ads/Ads.h
@@ -0,0 +1,18 @@
+//
+//  Ads.h
+//  Ads
+//
+//  Created by Castcle Co., Ltd. on 31/1/2565 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Ads.
+FOUNDATION_EXPORT double AdsVersionNumber;
+
+//! Project version string for Ads.
+FOUNDATION_EXPORT const unsigned char AdsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ads/PublicHeader.h>
+
+
